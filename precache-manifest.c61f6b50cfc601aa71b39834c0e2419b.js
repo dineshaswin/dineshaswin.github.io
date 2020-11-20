7176a5146f53096c56c356878d5400e21666d9cc
dineshaswin <aswin@billionlives.in>
2020-11-21 02:01:24 +0530

    gh-pages

---
+++ precache-manifest.c61f6b50cfc601aa71b39834c0e2419b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6199ebe0f1b56d13195",
+    "url": "/portfolio/css/chunk-vendors.40a641c3.css"
+  },
+  {
+    "revision": "07d1e22e8cddd99a53037dc510221b7b",
+    "url": "/portfolio/img/logo.07d1e22e.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/portfolio/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2be0d8b4a7bc940279154a20a4c4e3d3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7d806415b8bef85b2109",
+    "url": "/portfolio/js/about.207ec490.js"
+  },
+  {
+    "revision": "5e9fa4ce6f98ab80a417",
+    "url": "/portfolio/js/app.610bdb01.js"
+  },
+  {
+    "revision": "b6199ebe0f1b56d13195",
+    "url": "/portfolio/js/chunk-vendors.9017d965.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  }
+]);
